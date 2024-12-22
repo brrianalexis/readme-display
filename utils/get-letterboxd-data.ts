8b1bc7e19d848a4f7562087f857b665b819c666a
Brian Alexis Michel <51463930+brrianalexis@users.noreply.github.com>
2024-12-22 03:07:52 -0300

    fix: encode letterboxd entries images to base 64

--- utils/get-letterboxd-data.ts
+++ utils/get-letterboxd-data.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import Parser from "rss-parser";
 import { LetterboxdEntry } from "@/types";
 
@@ -8,10 +9,27 @@ export const getLetterboxdData = async (): Promise<LetterboxdEntry[]> => {
     `https://letterboxd.com/${process.env.LETTERBOXD_USERNAME}/rss/`
   );
 
-  return feed.items.slice(0, 3).map((item) => ({
-    title: item.title,
-    link: item.link,
-    pubDate: item.pubDate,
-    image: item.content?.match(/<img src="([^"]+)"/)?.[1],
-  })) as LetterboxdEntry[];
+  const entries = await Promise.all(
+    feed.items.slice(0, 3).map(async (item) => {
+      const imageUrl = item.content?.match(/<img src="([^"]+)"/)?.[1];
+      let encodedImage = "";
+
+      if (imageUrl) {
+        const image = await axios.get(imageUrl, {
+          responseType: "arraybuffer",
+        });
+        const raw = Buffer.from(image.data).toString("base64");
+        encodedImage = `data:${image.headers["content-type"]};base64,${raw}`;
+      }
+
+      return {
+        title: item.title,
+        link: item.link,
+        pubDate: item.pubDate,
+        image: encodedImage,
+      } as LetterboxdEntry;
+    })
+  );
+
+  return entries;
 };
