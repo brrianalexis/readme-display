87e22833d8aa0b5ecc26cf449e8eb63767ae89fd
Brian Alexis Michel <51463930+brrianalexis@users.noreply.github.com>
2024-12-22 02:56:56 -0300

    fix: add a build step for tailwind

--- utils/get-styles.ts
+++ utils/get-styles.ts
@@ -1,54 +1,21 @@
-import postcss from "postcss";
-import tailwindcss from "tailwindcss";
+import fs from "fs/promises";
+import path from "path";
 
 export async function getStyles() {
-  const css = `
-    @tailwind base;
-    @tailwind components;
-    @tailwind utilities;
-  
-    #bars {
-      height: 30px;
-      position: relative;
-      width: 40px;
-    }
+  const cssPath = path.join(process.cwd(), "styles", "output.css");
+  const css = await fs.readFile(cssPath, "utf-8");
 
-    .bar {
-      background: #1DB954cc;
-      bottom: 1px;
-      height: 3px;
-      position: absolute;
-      width: 3px;
-      animation: sound 0ms -800ms linear infinite alternate;
-    }
-
-    @keyframes sound {
-      0% {
-        opacity: .35;
-        height: 3px;
-      }
-
-      100% {
-        opacity: 1;
-        height: 15px;
-      }
-    }
-
-    ${Array.from({ length: 84 })
+  return (
+    css +
+    Array.from({ length: 84 })
       .map(
         (_, i) => `
-          .bar:nth-child(${i + 1}) {
-            left: ${i * 4}px;
-            animation-duration: ${Math.floor(Math.random() * 300) + 1000}ms;
-          }
-        `
+        .bar:nth-child(${i + 1}) {
+          left: ${i * 4}px;
+          animation-duration: ${Math.floor(Math.random() * 300) + 1000}ms;
+        }
+      `
       )
-      .join("")}
-  `;
-
-  const result = await postcss([tailwindcss]).process(css, {
-    from: undefined,
-  });
-
-  return result.css;
+      .join("")
+  );
 }
