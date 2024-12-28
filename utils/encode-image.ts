379d041a93f828a865c800c56086ab54e9f951ee
Brian Alexis Michel <51463930+brrianalexis@users.noreply.github.com>
2024-12-28 17:58:49 -0300

    refactor: replace fetch with axios

--- utils/encode-image.ts
+++ utils/encode-image.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const EMPTY_IMAGE =
   "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7";
 
@@ -5,10 +7,12 @@ export const encodeImage = async (url: string): Promise<string> => {
   if (!url) return EMPTY_IMAGE;
 
   try {
-    const response = await fetch(url);
-    const arrayBuffer = await response.arrayBuffer();
-    const base64 = Buffer.from(arrayBuffer).toString("base64");
-    const contentType = response.headers.get("content-type") || "image/jpeg";
+    const response = await axios.get(url, {
+      responseType: "arraybuffer",
+    });
+
+    const base64 = Buffer.from(response.data).toString("base64");
+    const contentType = response.headers["content-type"] || "image/jpeg";
     return `data:${contentType};base64,${base64}`;
   } catch (error) {
     console.error("Error encoding image:", error);
