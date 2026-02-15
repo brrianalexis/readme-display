482f4d4b9ea468e70d8942002a460b02a0c87a0a
Brian Alexis Michel <51463930+brrianalexis@users.noreply.github.com>
2026-02-15 15:45:23 -0300

    refactor: use constants in getStyles

--- utils/get-styles.ts
+++ utils/get-styles.ts
@@ -1,18 +1,25 @@
 import fs from "node:fs/promises";
 import path from "node:path";
 
+import { SVG_CONFIG } from "@/constants";
+
+const { count, min_duration, max_duration } =
+	SVG_CONFIG.lastfm.now_playing.animation.bars;
+
 export async function getStyles() {
 	const cssPath = path.join(process.cwd(), "styles", "output.css");
 	const css = await fs.readFile(cssPath, "utf-8");
 
+	const range = max_duration - min_duration + 1;
+
 	return (
 		css +
-		Array.from({ length: 84 })
+		Array.from({ length: count })
 			.map(
 				(_, i) => `
         .bar:nth-child(${i + 1}) {
           left: ${i * 4}px;
-          animation-duration: ${Math.floor(Math.random() * 300) + 1000}ms;
+          animation-duration: ${Math.floor(Math.random() * range) + min_duration}ms;
         }
       `,
 			)
